613a50d4e638abf03d500785197a6642cf51b8b2
SHRUTIBIRRU <90187675+SHRUTIBIRRU@users.noreply.github.com>
2021-10-27 09:35:33 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState ,useEffect} from "react";
+import WordBinder from "./Components/WordBinder";
+import SearchIcon from "@material-ui/icons/Search";
+import "./styles.css";
 
-function App() {
+export default function App() {
+  const [inputWord, setWord] = useState("");
+  const [searchWord, setSearchWord] = useState("Nice");
+  const RandWordApi = "https://api.wordnik.com/v4/words.json/randomWords?hasDictionaryDef=true&minCorpusCount=0&minLength=5&maxLength=15&limit=1&api_key=mhvfzxrhb41uag20wv1ycfl20yxq6f5zabdt82eutc2wuf1qf"
+  const [randWord,setRandDay] = useState("run");
+  
+  async function fetchRandWord() {
+    const res = await fetch(RandWordApi);
+    res
+      .json()
+      .then((res) => {
+        setRandDay(res[0].word)
+      })
+      .catch((err) => console.log(err));
+  }
+  useEffect(()=>{
+    fetchRandWord();
+  },[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="word-day">
+        <h2>Word of The Day : </h2>
+        <WordBinder searchedWord={randWord} limit="5" />
+      </div>
+      <label className="sub-container input-style">
+        <input
+          placeholder="Enter any word to search"
+          onChange={(e) => setWord(e.target.value)}
+          type="text"
+        />
+        <button onClick={(e) => setSearchWord(inputWord)}>
+          <SearchIcon />
+        </button>
+      </label>
+      <WordBinder searchedWord={searchWord} limit="10" />
     </div>
+    
+   
   );
 }
-
-export default App;
